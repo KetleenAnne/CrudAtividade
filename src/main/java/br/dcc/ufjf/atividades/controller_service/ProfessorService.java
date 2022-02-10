9c07f2b48664096c4b383bcf49536dddaf58b69f
0ket <ketleen@ice.ufjf.br>
2022-02-10 12:52:56 -0300

    service Professor Criado

---
+++ src/main/java/br/dcc/ufjf/atividades/controller_service/ProfessorService.java
@@ -0,0 +1,39 @@
+package br.dcc.ufjf.atividades.controller_service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import br.dcc.ufjf.atividades.model.Professor;
+import br.dcc.ufjf.atividades.repository.RepositoryProfessor;
+
+@Service
+public class ProfessorService {
+        // cria o repository Professor
+        @Autowired
+        private RepositoryProfessor repositoryProfessor;
+     
+        // lista todos os Professores
+        //@GetMapping
+        public List<Professor> getProfessores(){
+            return repositoryProfessor.findAll();
+        }
+    
+        // salva um novo Professsor
+        public Professor saveRegistration(@RequestBody Professor professor){
+           return repositoryProfessor.save(professor);
+    
+        }
+    
+        // procura um Professor a partir do seu id
+        public Professor get(Long id){
+            return repositoryProfessor.findById(id).get();
+        }
+    
+        // deleta Professor a partir do seu id
+        public void delete(Long id){ 
+            repositoryProfessor.deleteById(id);
+        }
+}
